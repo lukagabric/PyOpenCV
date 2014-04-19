831ea13910b289457e6b2e899ef83c924097498e
Luka Gabric <luka.gabric@gmail.com>
2014-04-19 17:04:56 +0200

    initial import

---
+++ PyOpenCVPackage/PyOCVCircle.py
@@ -0,0 +1,77 @@
+import cv2
+import cv2.cv as cv
+import numpy as np
+import sys
+        
+     
+if __name__ == "__main__":
+ 
+    print "Press ESC to exit ..."
+ 
+    # create windows
+    cv.NamedWindow('Original', cv.CV_WINDOW_AUTOSIZE)
+    cv.NamedWindow('Threshold', cv.CV_WINDOW_AUTOSIZE)
+ 
+    # create capture device
+    capture = cv.CreateCameraCapture(0) # assume we want first device
+    cv.SetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_WIDTH, 640)
+    cv.SetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_HEIGHT, 480)
+ 
+    # check if capture device is OK
+    if not capture:
+        print "Error opening capture device"
+        sys.exit(1)
+        
+    while 1:
+        # do forever
+ 
+        #capture the current frame
+        frame = cv.QueryFrame(capture)
+        
+        if frame is None:
+            break
+ 
+        # mirror
+        cv.Flip(frame, None, 1)
+        
+        originalImage = frame
+ 
+        hsvImage = cv.CreateImage(cv.GetSize(originalImage), 8, 3)
+        cv.CvtColor(originalImage, hsvImage, cv.CV_BGR2HSV)
+                            
+        thresholdImage = cv.CreateImage(cv.GetSize(originalImage), 8, 1)
+        cv.InRangeS(hsvImage, cv.Scalar(20.74, 75, 75), cv.Scalar(30.74, 255, 255), thresholdImage)
+                                       
+        thresholdImageArray = np.asarray(cv.GetMat(thresholdImage))
+        thresholdImageArray = cv2.GaussianBlur(thresholdImageArray, (0,0), 2)
+
+        thresholdImage = cv.fromarray(thresholdImageArray)
+        
+        circles = cv2.HoughCircles(thresholdImageArray,cv.CV_HOUGH_GRADIENT,2,10,param1=40,param2=80,minRadius=4,maxRadius=1500)
+        
+        if circles is not None:
+            if circles.size > 0:
+                circles = np.uint16(np.around(circles))
+            
+            for i in circles[0,:]:
+                imageArray = np.asarray(cv.GetMat(originalImage))
+                # draw the outer circle
+                cv2.circle(imageArray,(i[0],i[1]),i[2],(0,255,0),2)
+                # draw the center of the circle
+                cv2.circle(imageArray,(i[0],i[1]),2,(0,0,255),3)
+                print "center = (%d, %d)", i[0], i[1]
+        
+        # display webcam image
+        cv.ShowImage('Original', originalImage)
+        cv.ShowImage('Threshold', thresholdImage)
+ 
+        # handle events
+        #    As long as camera window has focus (e.g. is selected), this will intercept
+        #        pressed key; it will not work if the python terminal window has focus
+        k = cv.WaitKey(100)
+         
+        if k == 0x1b: # ESC
+            print 'ESC pressed. Exiting ...'
+            cv.DestroyWindow("Original")  # This may not work on a Mac
+            cv.DestroyWindow("Threshold")  # This may not work on a Mac
+            break
---
+++ PyOpenCVPackage/PyOCVSmile.py
@@ -0,0 +1,101 @@
+'''
+
+    A demonstration for face and object detection using haar-like features.
+    Faces are found in a video stream and a red box is drawn around them.
+    
+    Because of a bug in OpenCV on Mac OS X, the camera window may not close
+    after the program is ended, in which case you will need to
+    "Force Quit" Python.
+
+Modified (30 July 2011) by Donald J. Woodbury
+                            and David C. Bailey (dbailey@physics.utoronto.ca)
+    from the example by Jo Vermeulen retrieved from
+        http://blog.jozilla.net/2008/06/27/fun-with-python-opencv-and-face-detection/,
+        (License: Not known)
+    which in turn is based on a script by Nirav Patel from
+        http://eclecti.cc/olpc/face-detection-on-the-olpc-xo
+        (License: http://creativecommons.org/licenses/by/3.0/us/)
+    which is derived from the facedetect.py example by Roman Stanchak & James Bowman,
+         available from http://sourceforge.net/projects/opencvlibrary/.
+    OpenCV is available under the BSD License (http://opencv.willowgarage.com/wiki/).
+    The modifications by Woodbury and Bailey are released
+        under the MIT License - http://www.opensource.org/licenses/mit-license.php.
+
+    '''
+
+import sys
+import cv2.cv as cv
+
+def detect_and_draw(image):
+    image_size = cv.GetSize(image)
+
+    # convert color input image to rgb
+    cvImage = cv.CreateImage(image_size, 8, 1)
+    cv.CvtColor(image, cvImage, cv.CV_BGR2GRAY)
+    
+
+    # create storage
+    storage = cv.CreateMemStorage(0)
+
+    cv.EqualizeHist(cvImage, cvImage)
+  
+    # detect objects
+    
+    # locate relevant haar cascade data
+    haarlocation = '/Users/lukagabric/Workspaces/Kepler/Workspaces/PyOpenCV/smile.xml' # Mac OS X example
+    
+    # load haar cascade data
+    cascade = cv.Load(haarlocation)
+    
+    smiles = cv.HaarDetectObjects(cvImage, cascade, storage, 1.1, 3, cv.CV_HAAR_DO_CANNY_PRUNING)
+ 
+    if smiles:
+        print 'smiles detected!    Press Escape to exit program.'
+        for i in smiles:
+            cv.Rectangle(image, (int(i[0][2])+int(i[0][0]), int(i[0][3])+int(i[0][1])),
+                                    (int(i[0][0]), int(i[0][1])), (0, 255, 0), 3, 8, 0)
+ 
+if __name__ == "__main__":
+ 
+    print "Press ESC to exit ..."
+ 
+    # create windows
+    cv.NamedWindow('Camera', cv.CV_WINDOW_AUTOSIZE)
+ 
+    # create capture device
+    capture = cv.CreateCameraCapture(0) # assume we want first device
+    cv.SetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_WIDTH, 640)
+    cv.SetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_HEIGHT, 480)
+ 
+    # check if capture device is OK
+    if not capture:
+        print "Error opening capture device"
+        sys.exit(1)
+ 
+    while 1:
+        # do forever
+ 
+        #capture the current frame
+        frame = cv.QueryFrame(capture)
+        
+        if frame is None:
+            break
+ 
+        # mirror
+        cv.Flip(frame, None, 1)
+ 
+        # face detection
+        detect_and_draw(frame)
+ 
+        # display webcam image
+        cv.ShowImage('Camera', frame)
+ 
+        # handle events
+        #    As long as camera window has focus (e.g. is selected), this will intercept
+        #        pressed key; it will not work if the python terminal window has focus
+        k = cv.WaitKey(100)
+         
+        if k == 0x1b: # ESC
+            print 'ESC pressed. Exiting ...'
+            cv.DestroyWindow("Camera")  # This may not work on a Mac
+            break
